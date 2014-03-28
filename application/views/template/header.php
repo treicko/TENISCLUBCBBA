bba7ef84585c0cd185a11611cb2db373adec3ccf
ruby -v <leo_mejia_p@hotmail.com>
2014-03-28 10:30:24 -0400

    subiendo pagina vacion solo con bootstrap 2

---
+++ application/views/template/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $title; ?></title>    
+	<link href="<?php echo base_url(); ?>dist/css/bootstrap.css" rel="stylesheet" media="screen">
+	<link href="<?php echo base_url(); ?>dist/css/bootswatch.min.css" rel="stylesheet">
+	<!-- <link href="<?php #echo base_url(); ?>dist/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
+
+	<!--<link href="<?php #echo base_url(); ?>dist/css/bootstrap-theme.css" rel="stylesheet" >-->
+	<!--<link href="<?php #echo base_url(); ?>dist/css/bootstrap-theme.min.css" rel="stylesheet" >-->
+	<!-- <script type="text/javascript" src="<?php #echo base_url(); ?>dist/js/jquery-1.10.2.min.js"></script> -->
+
+	<!--<style>
+		table,th,td
+		{
+		border:1px solid black;
+		border-collapse:collapse;
+		}
+	</style>-->
+
+	<script>
+
+     var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23019901-1']);
+      _gaq.push(['_setDomainName', "bootswatch.com"]);
+        _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+     (function() {
+       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();
+
+    </script>
+
+</head>
+
+<body>
+	<script src="<?php echo base_url(); ?>dist/js/bsa.js"></script>
